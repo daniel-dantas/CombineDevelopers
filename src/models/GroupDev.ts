221a5d02c5c0a8cd3b0e90784dadc6105849f29e
daniel-dantas <daniel.dantas.developer@gmail.com>
2020-02-13 21:29:56 -0300

    GroupDev model creation

---
+++ src/models/GroupDev.ts
@@ -0,0 +1,30 @@
+import { Schema, model, Document } from 'mongoose'
+import { UserType } from './User'
+
+interface GroupDevType extends Document{
+  nameProject: string,
+  languagesStack: string[]
+  devs?: UserType[]
+}
+
+const groupSchema = new Schema({
+  nameProject: {
+    type: String,
+    required: true
+  },
+  languagesStack: {
+    type: [String],
+    required: true
+  },
+  devs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+})
+
+export default model('GroupDev', groupSchema)
+export {
+  GroupDevType
+}
